ad44c82e2f07db9d244b8f34c25564012e8eac79
东方月初 <165906706+damizai@users.noreply.github.com>
2025-05-14 02:02:15 +0800

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -8,14 +8,14 @@ const email_domains = env.NEXT_PUBLIC_EMAIL_DOMAINS || "";
 const email_r2_domain = env.NEXT_PUBLIC_EMAIL_R2_DOMAIN || "";
 
 export const siteConfig: SiteConfig = {
-  name: "WR.DO",
+  name: "BiuXin",
   description:
     "Shorten links with analytics, manage emails and control subdomains—all on one platform.",
   url: site_url,
   ogImage: `${site_url}/_static/og.jpg`,
   links: {
     twitter: "https://twitter.com/yesmoree",
-    github: "https://github.com/oiov/wr.do",
+    github: "https://github.com/damizai/wr.do-1",
     feedback: "https://github.com/oiov/wr.do/issues",
     discord: "https://discord.gg/AHPQYuZu3m",
     oichat: "https://oi.wr.do",
@@ -34,7 +34,7 @@ export const footerLinks: SidebarNavItem[] = [
       { title: "About", href: "/docs" },
       { title: "Terms", href: "/terms" },
       { title: "Privacy", href: "/privacy" },
-      { title: "Blog", href: "https://www.oiov.dev" },
+      { title: "Blog", href: "https://blog.biuxin.com" },
       { title: "Feedback", href: siteConfig.links.feedback },
     ],
   },
